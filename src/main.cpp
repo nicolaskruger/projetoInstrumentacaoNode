b3ea6cfea32613114dc57ced0171a88c8d9bd81a
nicolas-kruger <nicolas.nkruger@gmail.com>
2020-08-10 14:28:48 -0300

    03

--- src/main.cpp
+++ src/main.cpp
@@ -4,11 +4,17 @@
 
 #define LED D0
 
+void serverConection();
+void setUpW();
+
+void clientConnection();
+void clientMsg();
 bool b=false;
 
-int port = 8888;
+int port = 4242;
 WiFiServer server(port);
-
+WiFiClient client;
+IPAddress ip(10,42,0,1);
 const char *ssid = "myPc";
 const char *password = "duV5X0eE";
 
@@ -17,28 +23,9 @@ const char *password = "duV5X0eE";
 void setup() {
   pinMode(LED,OUTPUT);
   Serial.begin(9600);
-  Serial.println("teste\n");
-  WiFi.mode(WIFI_STA);
-  WiFi.begin(ssid,password);
-  // put your setup code here, to run once:
-  Serial.println("Connecting to Wifi");
-  while (WiFi.status() != WL_CONNECTED) {   
-    delay(500);
-    Serial.print(".");
-    delay(500);
-  }
- 
-  Serial.println("");
-  Serial.print("Connected to ");
-  Serial.println(ssid);
- 
-  Serial.print("IP address: ");
-  Serial.println(WiFi.localIP());  
-  server.begin();
-  Serial.print("Open Telnet and connect to IP:");
-  Serial.print(WiFi.localIP());
-  Serial.print(" on port ");
-  Serial.println(port);
+  //Serial.println("teste\n");
+  setUpW();
+  clientConnection();
 }
 
 void loop() {
@@ -46,6 +33,11 @@ void loop() {
   //digitalWrite(LED, b);
   //Serial.println("teste\n");
   //delay(1000);
+  clientMsg();
+  // put your main code here, to run repeatedly:
+}
+
+void serverConection(){
   WiFiClient client = server.available();
   
   if (client) {
@@ -68,5 +60,51 @@ void loop() {
     client.stop();
     Serial.println("Client disconnected");    
   }
-  // put your main code here, to run repeatedly:
+}
+void setUpW(){
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid,password);
+  // put your setup code here, to run once:
+  Serial.println("Connecting to Wifi");
+  while (WiFi.status() != WL_CONNECTED) {   
+    delay(500);
+    Serial.print(".");
+    delay(500);
+  }
+ 
+  Serial.println("");
+  Serial.print("Connected to ");
+  Serial.println(ssid);
+ 
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());  
+  server.begin();
+  Serial.print("Open Telnet and connect to IP:");
+  Serial.print(WiFi.localIP());
+  Serial.print(" on port ");
+  Serial.println(port);
+}
+void clientConnection(){
+  client.stop();
+  while (!client.connected())
+  {
+    /* code */
+    if(client.connect(ip,port)){
+      Serial.println("connected");
+    }
+    else{
+      Serial.println("not connected");
+    }
+    delay(1000);
+  }
+  
+}
+void clientMsg(){
+  client.println("connectado");
+  delay(1000);
+  while(client.available()>0){
+        // read data from the connected client
+        Serial.write(client.read()); 
+      }
+
 }
